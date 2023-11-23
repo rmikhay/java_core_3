9f18ada36f50445dc1e0d7dd51e3db6e2edd52ed
YMolodyko <VTB4081847@region.vtb.ru>
2023-11-23 21:27:35 +0300

    lesson 2

--- src/org/example/lessons/Lesson1.java
+++ src/org/example/lessons/Lesson1.java
@@ -1,17 +1,101 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+package org.example.lessons;
+
+public class Lesson1 {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Булево значение (истина/ложь)
+        boolean isGoToShop = true; //1
+        boolean isNotGoToShop = false; //0
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //ЦЕЛЫЕ ЧИСЛА
+        //bit - кусочек, число от 0 до 1
+        //byte - из 8 bit
+        //2 4 8 16 32 64 128 256
+        //1 1 1 1  1   1  1   1
+        //0 - 255
+        //-128 - 0 - 127
+        byte ipAddressFirst = -128;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        //short - состоит из двух байтов
+        //16bit
+        //256 * 256
+        //0 - 65535
+        //-32768 - 0 - 32767
+        short shortType = 32767;
+
+        //int - состоит из четырех байт
+        //32bit
+        //4 294 967 296
+        //-2 147 483 648 - 0 - 2 147 483 647
+        int countPeopleChina = 2_147_483_647;
+
+        //long - состоит из 8 байт
+        //64bit
+        long countWorldPeople = 8_000_000_000L;
+
+        //Числа с плавающей точкой
+        //float
+        //32bit
+        float cola = 0.12345678901234F;
+
+        //double
+        //64bit
+        double atom = 0.12345678901234567890;
+
+        int apple = 5;
+        // сложение
+        apple = 5 + 1;
+        //Вычитание
+        apple = 5 - 1;
+        //Деление
+        apple = 8 / 3;
+        //Умножение
+        apple = 2 * 2;
+
+        //Остаток от деления
+        apple = 33 % 2;
+        //33 % 2 = 33 - 32 = 1
+        //10 % 3 = 10 - 9 = 1
+
+
+        //Символ - char
+        //16bit
+        char symbol = 200;
+        char symbol2 = 'л';
+        System.out.printf("Печать символа: %s\n", symbol);
+
+        //String (Текст)
+        String name = "Юрий"; //02x7762
+        String name2 = new String("Юрий"); //02x7762
+        //Сравнение строк
+        System.out.printf("Сравнение строк через equals: %s\n", name.equals(name2));
+        System.out.printf("Сравнение строк через == (по ссылке): %s\n", name.equals(name2));
+
+        //Перевод в верхний регистр
+        String nameLower = name.toUpperCase();
+        System.out.printf("Перевод в верхний регистр: %s\n", nameLower);
+
+
+        //Перевод в нижний регистр
+        System.out.printf("Перевод в нижний регистр: %s\n", name.toLowerCase());
+
+        //Поиск по строке
+        boolean containsRi = name.contains("ри");
+        System.out.printf("Содержит строку 'ри': %s\n", containsRi);
+
+        //Повторение строки
+        String repeat = name.repeat(10);
+        System.out.printf("Повторение строки 10 раз: %s\n", repeat);
+
+        //Обрезка строки (от номера буквы до номера буквы)
+        String substring = name.substring(2);
+        System.out.printf("Обрезка строки (все символы после 2 символа): %s\n", substring);
+
+        //Получение длины строки (сколько букв)
+        int length = name.length();
+        System.out.printf("Длина строки: %s\n", length);
+
+        //Обрезка пробелов
+        String text = " some text    ";
+        System.out.printf("Обрезка пробелов: %s\n", text.trim());
     }
 }
--- src/org/example/lessons/Lesson2.java
+++ src/org/example/lessons/Lesson2.java
@@ -1,2 +1,65 @@
-package org.example.lessons;public class Lesson2 {
+package org.example.lessons;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        int money = 40;
+        int priceBread = 50;
+        int milk = 20;
+        boolean condition = false;
+        if (false ) {
+            System.out.println("Покупаю хлеб");
+        } else if (milk < money) {
+            System.out.println("Покупаю молоко");
+        } else {
+            System.out.println("Хлеб не покупаю и молоко тоже не покупаю");
+        }
+        //Операторы числового сравнения
+        // > - больше
+        // < - меньше
+        // >= - больше или равно
+        // <= - меньше или равно
+        // == - ровно такое же число
+        // != - число не равно второму числу
+
+        // && - Логическое "и" - два условия должны быть true, иначе false
+        if (milk == 30 && priceBread == 20) {
+            System.out.println("Покупаю и хлеб и молоко");
+        }
+
+        // || - Логическое или - одно из условий должно быть true -> тогда true, иначе false
+        // & и | - побитовые операции
+        // число 2 в десятичной системе
+        // 10
+        // число 3 в десятичной системе
+        // 11
+        // 2 & 3  - 10
+        // 2 | 3 - 11
+
+        //Составление логических цепочек
+        int childAge1 = 5;
+        int childAge2 = 10;
+        int childAge3 = 15;
+        // (false)          || (false)          || (false)
+        // false || (false)
+        // false
+        if ( childAge1 >= 18 || childAge2 >= 18 || childAge3 >= 18 ) {
+            System.out.println("Открываю шампанское");
+        }
+
+        //Логическое "Не" - переворачивает значение
+        boolean isGoToSchool = !true;
+        System.out.println(isGoToSchool);
+
+        String name = new String("Юрий");
+        if (!name.contains("Ю")) {
+            System.out.println("Имя не содержит букву Ю");
+        }
+
+        String text = null;
+        text.toLowerCase();
+        System.out.println(text);
+        text = "какой то текст";
+        System.out.println(text);
+
+    }
 }
